21e5178fd480693cbc62a9156b22afadf6847cb1
Jennifer Beem and Xia Amendolara <xia@>
2017-02-13 15:12:25 -0800

    fail to debug front end logic

--- app/app.php
+++ app/app.php
@@ -1,12 +1,22 @@
 <?php
-    // date_default_timezone_set('America/Los_Angeles');
-    // require_once __DIR__."/../vendor/autoload.php";
-    //
-    // $app = new Silex\Application();
-    //
-    // $app->get("/", function() {
-    //     echo $output_num;
-    // });
-    //
-    // return $app;
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/PingPongGenerator.php";
+
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__.'/../views']);
+
+    $app->get("/", function() use($app) {
+        return $app['twig']->render('ping-pong.html.twig');
+    });
+
+    $app->post('/result', function() use($app) {
+        $number = new PingPongGenerator($_POST['number']);
+        // $number->makePingPong($number);
+        // $new_num = $number->getNumber();
+        return $app['twig']->render('ping-pong-result.html.twig', ["result" => $number]);
+    });
+    return $app;
 ?>
--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -1,23 +1,53 @@
 <?php
 
-    class PingPongGenerator
+class PingPongGenerator
+{
+    public $number;
+
+    function __construct($number)
     {
-        function makePingPong($input_num)
-        {
-            // $output_num = [];
-            // for ($i = 1; $i <= $input_num; $i++)
-                if (($input_num % 15 == 0)) {
-                    return "ping-pong";
-                }
-                if (($input_num % 3 == 0)) {
-                    return "ping";
-                }
-                if (($input_num % 5 == 0)) {
-                    return "pong";
-                }
-                    // $i == "ping";
-                // } array_push($output_num, $i);
-                // echo $output_num;
-        }
+        $this->number = $number;
     }
- ?>
+
+    function getNumber()
+    {
+        $this->number = $number;
+    }
+
+    function setNumber($new_number)
+    {
+        $this->number = $new_number;
+        return $new_number;
+    }
+
+    //
+    // function setNumer($number)
+    // {
+    //     $this->number = "";
+    // }
+
+    // function makePingPong($this_num)
+    // {
+    //     $number = $this_num;
+    //     $output_num = [];
+    //     for ($i = 1; $i <= $this_num; $i++) {
+    //         array_push($output_num, $i);
+    //     };
+    //     for ($i = 0; $i <sizeof($output_num); $i++)
+    //     {
+    //         if (($this_num % 15 == 0)) {
+    //             return "ping-pong";
+    //         }
+    //         if (($this_num % 3 == 0)) {
+    //             return "ping";
+    //         }
+    //         if (($this_num % 5 == 0)) {
+    //             return "pong";
+    //         }
+    //     }
+    //         return $output_num;
+    //         echo $output_num;
+    //
+    // }
+}
+?>
